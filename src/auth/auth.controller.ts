ca468d653b1a52a5556039f7fbe17bb245916358
v4n8877 <v4n8877@gmail.com>
2025-10-31 08:57:06 +0700

    fix: update config git

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('register')
   async register(@Body() createAuthDto: CreateAuthDto) {
     return await this.authService.register(createAuthDto);
